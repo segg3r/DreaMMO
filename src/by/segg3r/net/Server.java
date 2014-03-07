0f5088535728f62fe35c7358fe1247a4f77b1faa
segg3r <segg3r@gmail.com>
2014-03-07 15:31:18 +0300

    Game Character mapping to client implemented.

--- src/by/segg3r/net/Server.java
+++ src/by/segg3r/net/Server.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import by.segg3r.ServerApplicationContext;
 import by.segg3r.entities.GameCharacter;
@@ -17,7 +20,7 @@ public class Server extends Thread {
 
 	private ServerSocket serverSocket;
 	private List<Client> clients;
-	private List<GameCharacter> gameCharacters;
+	private Map<GameCharacter, Client> gameCharacterMapping;
 
 	/**
 	 * Instantiates a new server.
@@ -31,6 +34,7 @@ public class Server extends Thread {
 		super();
 		this.serverSocket = new ServerSocket(port);
 		this.clients = new ArrayList<Client>();
+		this.gameCharacterMapping = new HashMap<GameCharacter, Client>();
 
 		ServerApplicationContext.getLog().printMessage(
 				"Server initialized at port " + port);
@@ -82,6 +86,87 @@ public class Server extends Thread {
 	public void sendAllButOne(Client exceptedClient, AbstractTask task) {
 		for (Client client : clients) {
 			if (!client.equals(exceptedClient)) {
+				client.sendTask(task);
+			}
+		}
+	}
+
+	/**
+	 * Game character loginned.
+	 * 
+	 * @param gameCharacter
+	 *            the game character
+	 * @param client
+	 *            the client
+	 */
+	public void gameCharacterLogin(GameCharacter gameCharacter, Client client) {
+		this.gameCharacterMapping.put(gameCharacter, client);
+	}
+
+	/**
+	 * Game character logouted.
+	 * 
+	 * @param gameCharacter
+	 *            the game character
+	 */
+	public void gameCharacterLogout(GameCharacter gameCharacter) {
+		this.gameCharacterMapping.remove(gameCharacter);
+	}
+
+	/**
+	 * Checks if is client loginned.
+	 * 
+	 * @param client
+	 *            the client
+	 * @return true, if is client loginned
+	 */
+	public boolean isClientLoginned(Client client) {
+		return this.gameCharacterMapping.containsValue(client);
+	}
+
+	/**
+	 * Send task to all loginned.
+	 * 
+	 * @param task
+	 *            the task
+	 */
+	public void sendTaskToAllLoginned(AbstractTask task) {
+		for (Client client : this.gameCharacterMapping.values()) {
+			client.sendTask(task);
+		}
+	}
+
+	/**
+	 * Send task to all loginned but one.
+	 * 
+	 * @param exceptedClient
+	 *            the excepted client
+	 * @param task
+	 *            the task
+	 */
+	public void sendTaskToAllLoginnedButOne(Client exceptedClient,
+			AbstractTask task) {
+		for (Client client : this.gameCharacterMapping.values()) {
+			if (!client.equals(exceptedClient)) {
+				client.sendTask(task);
+			}
+		}
+	}
+
+	/**
+	 * Send task to all but one.
+	 * 
+	 * @param exceptedGameCharacter
+	 *            the excepted game character
+	 * @param task
+	 *            the task
+	 */
+	public void sendTaskToAllButOne(GameCharacter exceptedGameCharacter,
+			AbstractTask task) {
+		Set<GameCharacter> gameCharacters = this.gameCharacterMapping.keySet();
+		for (GameCharacter gameCharacter : gameCharacters) {
+			if (!gameCharacter.equals(exceptedGameCharacter)) {
+				Client client = this.gameCharacterMapping.get(gameCharacter);
 				client.sendTask(task);
 			}
 		}
--- src/by/segg3r/tasks/StreamInitializationTask.java
+++ src/by/segg3r/tasks/StreamInitializationTask.java
@@ -2,10 +2,18 @@ package by.segg3r.tasks;
 
 import by.segg3r.net.task.AbstractTask;
 
+/**
+ * The Class StreamInitializationTask.
+ */
 public class StreamInitializationTask extends AbstractTask {
 
 	private static final long serialVersionUID = -6791446819265535892L;
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see by.segg3r.net.task.AbstractTask#execute()
+	 */
 	@Override
 	public void execute() {
 		System.out.println("Socket stream initialized");
